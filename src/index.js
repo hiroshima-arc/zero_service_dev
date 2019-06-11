d6f04c9a0dc96cc8ef9890a487355db7bce8af18
k2works <kakimomokuri@gmail.com>
2019-06-11 12:21:46 +0900

    feat: 認証

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import {
+  signUpEvent,
+  verifyEvent,
+  signInEvent,
+  signOutEvent,
+  currentUser,
+  forgetEvent,
+  resetPasswordEvent
+} from "./javascript/auth";
+
+const SignUpButton = document.getElementById("sign-up");
+if (SignUpButton) SignUpButton.addEventListener("click", signUpEvent);
+
+const VerifyButton = document.getElementById("verify");
+if (VerifyButton) VerifyButton.addEventListener("click", verifyEvent);
+
+const SignInButton = document.getElementById("sign-in");
+if (SignInButton) SignInButton.addEventListener("click", signInEvent);
+
+const SignOutButton = document.getElementById("sign-out");
+if (SignOutButton) SignOutButton.addEventListener("click", signOutEvent);
+
+const ForgetButton = document.getElementById("forget");
+if (ForgetButton) ForgetButton.addEventListener("click", forgetEvent);
+
+const ResetButton = document.getElementById("reset");
+if (ResetButton) ResetButton.addEventListener("click", resetPasswordEvent);
+
+const signinUser = document.getElementById("signin-user");
+if (signinUser) currentUser();
+
+
