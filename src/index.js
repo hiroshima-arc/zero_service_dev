0ada7a4ad8193a9493be93f0310c987110916b50
k2works <kakimomokuri@gmail.com>
2019-06-12 15:14:17 +0900

    chore: Storage

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ import {
 } from "./javascript/auth";
 
 import { signUpButtonClickEvent1, signUpButtonClickEvent2, signInButtonClickEvent, signOutButtonClickEvent } from "./javascript/analytics";
-
 import "./javascript/api";
-
 import "./javascript/graphql";
 import "./javascript/appsync";
+import "./javascript/storage";
 
 const SignUpButton = document.getElementById("sign-up");
 if (SignUpButton) SignUpButton.addEventListener("click", signUpEvent);
